f2ec084e785b3082aec23e15563dba29e37bc480
Jesus Juarez <jjuarez@medicasur.org.mx>
2024-11-17 07:01:52 -0600

    Add button action recording/stop

--- UI.py
+++ UI.py
@@ -4,6 +4,16 @@ from tkinter import filedialog
 import cv2
 import mediapipe as mp
 
+
+"""
+Reference 
+https://github.com/google-ai-edge/mediapipe/blob/master/docs/solutions/face_detection.md
+https://github.com/google-ai-edge/mediapipe/blob/master/docs/solutions/face_detection.md#model_selection
+https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#a85b55cf6a4a50451367ba96b65218ba1
+https://docs.opencv.org/4.x/dd/d9e/classcv_1_1VideoWriter.html 
+https://omes-va.com/tkinter-opencv-video/ 
+
+"""
 def upload_video():
     #This funtions is used to open a video file
     file_path = filedialog.askopenfilename()
@@ -26,8 +36,17 @@ def upload_video():
         video.release()
         output_video.release()
 
+
+
 def record_video():
-    pass
+    global is_recording
+    if is_recording:
+        record_button.config(text = "Stop")
+
+    else:
+        record_button.config(text = "Record Video")
+
+    is_recording = not is_recording
 
 def import_model():
     mp_face = mp.solutions.face_detection.FaceDetection(
@@ -57,41 +76,43 @@ def process_img(frame, mp_face):
 
     return frame
 
-
+is_recording = False
 # Start main window
 root = tk.Tk()
 root.title("Face-Anom")
-root.geometry("400x300") 
+root.geometry("640x500") 
 
 #Make tittle 
 title_label = tk.Label(root, text = "FACE-ANOM", font = ("Helvetica", 16, "bold"))
-title_label.pack(pady=10)
+title_label.pack(pady=5)
 
 instructions = ( 
-    "Instructions: This desktop app allows you to anonymize faces in a video: "
-    "+ Use 'Upload Video' to load a video from your computer."
-    "+ Use 'Record Video' to capture a video from your webcam."
-    "+ The app will automatically blur detected faces."
+    "Instructions: This desktop app allows you to anonymize faces in a video: \n "
+    "+ Use 'Upload Video' to load a video from your computer. \n"
+    "+ Use 'Record Video' to capture a video from your webcam. \n"
+    "+ The app will automatically blur detected faces. \n"
     )
 
-
+instructions_label = tk.Label(root, text=instructions, justify="left", wraplength=350)
+instructions_label.pack(pady=5)
 #Make buttons 
 buttos_frame = tk.Frame(root)
-buttos_frame.pack(pady = 20 )
+buttos_frame.pack(pady = 5 )
 
 
 upload_button = tk.Button(buttos_frame, text = "Upload Video", width = 20, command = upload_video)
-upload_button.grid(row = 0, column = 0, padx = 10, pady = 10)
+upload_button.grid(row = 0, column = 0, padx = 10, pady = 5)
 
 record_button = tk.Button(buttos_frame, text = "Record Video", width = 20, command = record_video)
-record_button.grid(row = 0, column = 1, padx = 10, pady = 10)
+record_button.grid(row = 0, column = 1, padx = 10, pady = 5)
 
 #Video Window
-
+lblVideo = tk.Label(buttos_frame)
+lblVideo.grid(column = 0, row = 1, columnspan = 2)
 
 # Make footer
 
 footer_label = tk.Label(root, text = "CS50 Final Project", font = ("Helvetica", 10, "italic"))
-footer_label.pack(side = "bottom", pady = 10)
+footer_label.pack(side = "bottom", pady = 20)
 
 root.mainloop()
